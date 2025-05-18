580ca4ad14735c34c348cb56eb7bffcf23c60e58
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2025-05-17 23:21:32 -0300

    feat: criação do componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-    <div>App</div>
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
-export default App
+export default App;
